fff4e740072dbe679b0fc2d9a3ab9cbc0d0bbf31
PARK <bht9011@gmail.com>
2021-11-11 14:55:40 +0900

    learned useEffect

--- src/App.js
+++ src/App.js
@@ -1,15 +1,30 @@
 import { useState, useEffect } from "react";
 
+function Hello() {
+  function byFn() {
+    console.log("bye :(");
+  }
+  function hiFn() {
+    console.log("created :)");
+    return byFn;
+  }
+  useEffect(hiFn, []);
+
+  // function Hello() {
+  //   useEffect(() => {
+  //     console.log("hi :)");
+  //     return () => console.log("bye :(");
+  //   }, []);
+  return <h1>Hello</h1>;
+}
+
 function App() {
-  const [counter, setValue] = useState(0);
-  const onClick = () => setValue((prev) => prev + 1);
-  useEffect(() => {
-    console.log("CALL THE API...");
-  }, []);
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <h1>{counter}</h1>
-      <button onClick={onClick}>Click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
